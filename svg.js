f2c19adb41cfee321f6a5dbc89d2dfca485c3fba
Deepanshu Raghuvanshi <raghuvanshi.deepanshu9@gmail.com>
2022-09-06 23:47:08 -0500

    Delete svg.js

--- svg.js
+++
@@ -1,9 +0,0 @@
-function setActive(){
-  document.getElementById('myrect').setAttribute('class','active')
-  document.getElementById('text').innerHTML = "Great Job!"
-}
-
-function setInactive(){
-  document.getElementById('myrect').setAttribute('class','inactive')
-  document.getElementById('text').innerHTML = "Rollover the Square"
-}
